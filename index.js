3c059c4a099e544588e7fe70f4bd376c8f96884a
Maxime Nory <maximenory@yahoo.fr>
2018-10-18 01:02:54 +0200

    🤞

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import applyDecoratedDescriptor from "@babel/runtime/helpers/esm/applyDecoratedDescriptor";
+import initializerDefineProperty from "@babel/runtime/helpers/esm/initializerDefineProperty";
+import { name as appName } from "./app.json";
 
+Object.assign(babelHelpers, {
+  applyDecoratedDescriptor,
+  initializerDefineProperty
+});
+
+const App = require("./App").default;
 AppRegistry.registerComponent(appName, () => App);
