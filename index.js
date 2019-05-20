5bf1b820b8d140bb492582ad402bd08148741f0d
Maxime Nory <mail@maximenory.com>
2019-05-20 22:07:37 +0200

    upgrade to react-native 0.59.8

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 /** @format */
 
 import { AppRegistry } from "react-native";
-import applyDecoratedDescriptor from "@babel/runtime/helpers/esm/applyDecoratedDescriptor";
-import initializerDefineProperty from "@babel/runtime/helpers/esm/initializerDefineProperty";
+import App from "./App";
 import { name as appName } from "./app.json";
 
-Object.assign(babelHelpers, {
-  applyDecoratedDescriptor,
-  initializerDefineProperty
-});
-
-const App = require("./App").default;
 AppRegistry.registerComponent(appName, () => App);
