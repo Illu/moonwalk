65574bb881701902dd65ff2afd345a7acb321f2d
Maxime Nory <mail@maximenory.com>
2020-02-29 22:05:48 +0100

    add livestream link

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -4,3 +4,5 @@ export {default as Calendar} from './calendar.svg';
 export {default as Pin} from './pin.svg';
 export {default as News} from './book.svg';
 export {default as X} from './x.svg';
+export {default as Video} from './video.svg';
+export {default as VideoOff} from './video-off.svg';
--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import { useTheme } from '@react-navigation/native';
-import { ScrollView, View } from 'react-native';
-import BigTitle from '../common/BigTitle';
+import { ScrollView, Linking, View } from 'react-native';
 import Icon from '../common/Icon';
 import { useSafeArea } from 'react-native-safe-area-context';
 import Label from '../common/Label';
@@ -43,6 +42,16 @@ const DescText = styled.Text`
   font-size: 17px;
 `;
 
+const LinkButton = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ButtonTitle = styled.Text`
+  margin-left: 10px;
+`;
+
 const CloseIconWrapper = styled.TouchableOpacity`
   width: 40px;
   height: 40px;
@@ -62,6 +71,8 @@ const Details: React.FC<Props> = ({ route, navigation }) => {
   const { colors } = useTheme();
   const { data } = route.params;
   const inset = useSafeArea();
+  const videoLink = data.vidURLs.length > 0 && data.vidURLs[0];
+
   console.log(data);
 
   return (
@@ -79,20 +90,27 @@ const Details: React.FC<Props> = ({ route, navigation }) => {
             <Location style={{ color: colors.secondaryText }}>{data.location.name}</Location>
           </Row>
           {data.location.typeName && (
-          <Row>
-            <Icon name="Pin" />
-            <Location style={{ color: colors.secondaryText }}>{data.location.typeName}</Location>
-          </Row>
+            <Row>
+              <Icon name="Pin" />
+              <Location style={{ color: colors.secondaryText }}>{data.location.typeName}</Location>
+            </Row>
           )}
           {data.missions.map(mission => (
-            <View key={mission.id} style={{marginTop: 16}}>
-            {mission.typeName && (
-              <Label text={mission.typeName}/>
-            )}
-            <DescText style={{ color: colors.text }} key={mission.id}>{mission.description}</DescText>
+            <View key={mission.id} style={{ marginTop: 16 }}>
+              {mission.typeName && (
+                <Label text={mission.typeName} />
+              )}
+              <DescText style={{ color: colors.text }} key={mission.id}>{mission.description}</DescText>
             </View>
           ))}
           <Countdown wsstamp={data.wsstamp} />
+            <LinkButton
+              disabled={!videoLink}
+              onPress={() => Linking.openURL(videoLink)}
+            >
+              <Icon name={videoLink ? "Video" : "VideoOff"} />
+            <ButtonTitle style={{color: colors.text}}>{videoLink ? "Watch Livestream" : "Livestream unavailable"}</ButtonTitle>
+            </LinkButton>
         </ContentWrapper>
       </View>
     </ScrollView>
