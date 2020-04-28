4fbfb143d9d1fcd1d8bb59c845296dcaf50d8cdc
Maxime Nory <mail@maximenory.com>
2020-04-28 18:27:41 +0200

    enable MobX observer batching

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import { AppRegistry } from "react-native";
 import "react-native-gesture-handler";
+import "mobx-react-lite/batchingForReactNative";
 import App from "./App";
 import { name as appName } from "./app.json";
 
