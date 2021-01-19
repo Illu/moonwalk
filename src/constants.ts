b5856a5f45bdd28eef8617e6e4db28cec894b2cc
derkweijers <derk@weijers.xyz>
2021-01-19 12:17:59 +0100

    Bumped to LL 2.1.0

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,7 @@ export enum STATES {
 export const HEADER_HEIGHT = 50;
 export const TABBAR_HEIGHT = 70;
 
-export const API_URL = "https://ll.thespacedevs.com/2.0.0/";
+export const API_URL = "https://ll.thespacedevs.com/2.1.0/";
 
 export const NEWS_API_URL = "https://spaceflightnewsapi.net/api/v2/";
 
